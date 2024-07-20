3fff53d58c86612c8feda6bcc26df7efea32b321
Ankita <142251107+Ankitadp-73@users.noreply.github.com>
2024-07-20 10:10:19 +0000

    DataTypes

--- 01_basics/02_DataTypes.js
+++ 01_basics/02_DataTypes.js
@@ -2,6 +2,11 @@
 // alert(2+2);// we are using nodejs,not browser
 console.log("hello");
 
+
+// javaScript is dynamically typed language
+a="abc";
+a=10;
+
 //data Types
 //1.primitive
 //2.objects
@@ -16,9 +21,14 @@ console.log("hello");
 //undefined = not assigned the value yet
 //symbol = to find the uniqueness 
 
-//objects
-
+//objects // references[NOn primitive]
+//1 Arrays
+//Functions
+// object 
 console.log(typeof(123));
 console.log(typeof(null));  // type of null=object 
 console.log(typeof(undefined)); // type of undefined = undefended 
-
+console.log("++++++++++++==");
+a="abc";
+a=10;
+console.log(a);
