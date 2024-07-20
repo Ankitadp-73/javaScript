1cc4a81d0d20f4cce00caed1a55b48d2ea0ff3c9
Ankita <142251107+Ankitadp-73@users.noreply.github.com>
2024-07-20 07:09:37 +0000

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strick"; // treate all js code as newer version
+// alert(2+2);// we are using nodejs,not browser
+console.log("hello");
+
+//data Types
+//1.primitive
+//2.objects
+
+
+    // primitive
+// number= 2 to power of 53
+// bigInt
+//string= " "
+//boolean = true/false
+//null = standAlone value (empty )
+//undefined = not assigned the value yet
+//symbol = to find the uniqueness 
+
+//objects
+
+console.log(typeof(123));
+console.log(typeof(null));  // type of null=object 
+console.log(typeof(undefined)); // type of undefined = undefended 
+
