5d83402981f4a3b93297deadf704121bfa1a399f
Ankita <142251107+Ankitadp-73@users.noreply.github.com>
2024-07-20 09:00:36 +0000

    conversionOfDATATYPES

---
+++ 01_basics/03_conversionOperationsOfdataTpyes.js
@@ -0,0 +1,40 @@
+let score=33;
+console.log(typeof(score));
+let change= Number(score);
+
+console.log(typeof (change));
+
+let marks="33aa";
+console.log(typeof(marks));
+
+let store=Number(marks)
+console.log(typeof(store));
+console.log(store);
+
+let num='0';
+let nums=Number(num);
+console.log(nums);
+console.log(typeof(num));
+
+console.log("************");
+
+let num1=null;
+let nums1=Number(num1);
+console.log(nums1);
+console.log(typeof(num1));
+console.log(typeof(nums1));
+
+// boolean 0= false 1=true
+let isloggedIn;
+let boolean=Boolean(isloggedIn);
+console.log(boolean);
+
+// " " for empty= false and  for value= true
+
+
+console.log("**************");
+console.log("String");
+let somenum=44;
+let stringstr=String(somenum);
+console.log(stringstr);
+console.log(typeof(stringstr));
