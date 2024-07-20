0dc15fe20f0e781fb0f5bd97a264d662c6c792bb
Ankita <142251107+Ankitadp-73@users.noreply.github.com>
2024-07-20 07:07:55 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const  accountId=122
+let emailId="a@gmail.com";
+var password="1233"
+acountCity="Bangalore"
+// accountId=1
+console.log(accountId);
+
+emailId="b@gmail.ccom"
+password="222";
+acountCity="Bidar";
+let state;
+console.log(emailId);
+console.log([accountId,emailId,password,acountCity]);
+console.table([emailId,password,accountId,acountCity,state]);
+
+
+/**
+ * prefered not to use var,Because of issue in block scope and functional scope
+ */
