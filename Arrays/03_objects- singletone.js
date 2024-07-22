9e8495e33d9c3dfd2a600f381f4d709170847ac8
Ankita <142251107+Ankitadp-73@users.noreply.github.com>
2024-07-22 17:55:29 +0000

    Object-Singletone

---
+++ Arrays/03_objects- singletone.js
@@ -0,0 +1,14 @@
+//SingleTone objects
+// if we declare the object by using the new Object() key word 
+// then that object is called singleTone Object
+
+
+// const name=new Object()
+// console.log(name);
+
+
+const obj={
+    name:"ANKITA",
+    age:20
+}
+console.log(obj.name);
