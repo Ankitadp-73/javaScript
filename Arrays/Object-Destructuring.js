55969e2fc7ca886ab5d6869355039ba505436f89
Ankita <142251107+Ankitadp-73@users.noreply.github.com>
2024-07-23 17:55:38 +0000

    obj-De-structuring

---
+++ Arrays/Object-Destructuring.js
@@ -0,0 +1,14 @@
+const course={
+    coursename:"js for web!",
+    price:999,
+    courseInstructure:"Ankita"
+}
+
+const{courseInstructure}=course
+console.log(courseInstructure);
+
+
+//de-structuring
+const{courseInstructure:instructure}=course
+console.log(instructure);
+
